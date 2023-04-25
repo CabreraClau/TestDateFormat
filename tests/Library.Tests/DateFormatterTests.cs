94ac1097bfcd6bb64c486182f199abc00748607f
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2023-04-25 15:10:53 -0300

    TestSolucion

--- tests/Library.Tests/DateFormatterTests.cs
+++ tests/Library.Tests/DateFormatterTests.cs
@@ -12,24 +12,24 @@ public class DateFormatterTests
     {
         const string InputDate = "10/11/1997";
         const string ExpectDate = "1997-11-10";
-        string actualDate = DateFormatter.ChangeFormat(inputDate);
+        string actualDate = DateFormatter.ChangeFormat(InputDate);
         
         
         Assert.AreEqual(ExpectDate, actualDate);
     }
 
-    public void Test2()
+    public void TestlenghtIsValid()
     {
-        const string testDate = "10/11/1997";
-        const string expect = "1997-11-10";
+        const string testDate = "10/11/19977";
+        const string expect = "";
         Console.WriteLine($"{testDate} se convierte a {DateFormatter.ChangeFormat(testDate)}");
         Assert.Pass();
         Assert.AreEqual(expect,testDate);
     }
 
-    public void Test3()
+    public void TestDayIsValid()
     {
-        const string testDate = "";
+        const string testDate = "50/20/2011";
         const string expect = "";
         Console.WriteLine($"{testDate} se convierte a {DateFormatter.ChangeFormat(testDate)}");
         Assert.Pass();
